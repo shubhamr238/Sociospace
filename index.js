486a3c2c78764f975e5d408709754db6fd4a0312
shubhamr238 <shubhamr238@gmail.com>
2020-03-04 22:27:19 +0530

    setting up routes dir

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const port= process.env.PORT || 8000;
 const express= require("express");
 const app=express();
 
-
+//use express router
+app.use('/', require('./routes'));
 app.listen(port, (err)=>{
     if(err)
         console.log(`Error ${err}`);
     console.log(`Server Running at ${port}.`);
 
-})
+});
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express=require("express");
+const router=express.Router();
+
+
+console.log("sdfghjkl");
+
+
+module.exports=router;
