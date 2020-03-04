d8d83f844fd09fe34bec54554d5fb7e51233f2fb
shubhamr238 <shubhamr238@gmail.com>
2020-03-05 00:19:51 +0530

    Setting up ejs and rendering home.ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home=(req, res)=>{
-    return res.end("<h1>Sociospace up and running!</h1>")
-}
+    return res.render('home', {
+        title: "Sociospace | Home"
+    });
+};
--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app=express();
 
 //use express router
 app.use('/', require('./routes'));
+
+//view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.listen(port, (err)=>{
     if(err)
         console.log(`Error ${err}`);
