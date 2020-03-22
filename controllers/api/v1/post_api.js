4a20b20417dca2027a6af553fef783e83133c14a
shubhamr238 <shubhamr238@gmail.com>
2020-03-22 20:17:30 +0530

    passport jwt auth(auth using api, that returns a token and this token is then used to perfom some function, like delete post here)

--- controllers/api/v1/post_api.js
+++ controllers/api/v1/post_api.js
@@ -23,7 +23,7 @@ module.exports.destroy=async function(req, res){
     try {
         let post= await Post.findById(req.params.id);    
         //_id is object and .id is string
-        // if(post.user==req.user.id){
+        if(post.user==req.user.id){
             post.remove();
             
             await Comment.deleteMany({post:req.params.id});
@@ -33,10 +33,11 @@ module.exports.destroy=async function(req, res){
             return res.json(200,{
                 message: "Post and Associated Commenets Deleted Sucessfully!",
             });
-        // }else{
-        //     req.flash('error', "You Cannot Delete this Post!");
-        //     return res.redirect('back');
-        // }
+        }else{
+            return res.json(401,{
+                message: "You cannot delete this post!"
+            });
+        }
     } catch (err) {
         // req.flash('error', err);
         console.log(err);
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const session=require('express-session');//used for session cookie
 const expressLayouts= require("express-ejs-layouts");
 const passport=require('passport');
 const passportLocal= require('./config/passport-local-strategy');
+const passportJWT=require('./config/passport-jwt-strategy');
 const MongoStore=require('connect-mongo')(session);
 const sassMiddleware=require('node-sass-middleware');
 const flash=require('connect-flash');
